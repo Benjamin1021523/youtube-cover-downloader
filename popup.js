584e4885866f56d5706a39fc4bd26703774940d1
Benjamin1021523 <yzu1021523@gmail.com>
2023-05-06 18:39:19 +0800

    1. 增加切換下載不同大小圖片的功能(部份影片最大圖片不存在) 2. 可自行修改檔名下載 3. 將部份功能改用共用函式

--- popup.js
+++ popup.js
@@ -1,29 +1,47 @@
 var title;
+var imgUrl;
+
+const setImageInfo = (ele) => {
+  document.getElementById('title').value = title + '_' + ele.textContent;
+  document.getElementById('preview').src = imgUrl + ele.id + '.jpg';
+  document.getElementById('download').href = imgUrl + ele.id + '.jpg';
+}
 
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
   const urlStr = tabs[0].url;
-  title = tabs[0].title;
-
-  document.getElementById('title').textContent = urlStr;
 
   const url = new URL(urlStr);
   const videoId = url.searchParams.get('v');
   if (!videoId) {
-    document.getElementById('title').textContent = '非執行對象';
-    document.getElementById('maxresdefault').hidden = true;
+    document.getElementById('info').textContent = '非執行對象';
+    document.getElementById('main').hidden = true;
     return;
   }
 
-  let imgUrl = `https://i.ytimg.com/vi/${videoId}/`;
+  title = tabs[0].title.slice(0, tabs[0].title.length - ' - YouTube'.length);
 
-  document.getElementById('maxresdefault').src = imgUrl + 'maxresdefault.jpg';
-  document.getElementById('download').href = imgUrl + 'maxresdefault.jpg';
-  document.getElementById('download').download = 'maxresdefault.jpg';
+  document.getElementById('info').textContent = title;
+
+  imgUrl = `https://i.ytimg.com/vi/${videoId}/`;
+
+  document.getElementById('maxresdefault').click();
+});
+
+document.getElementById('maxresdefault').addEventListener('click', function() {
+  setImageInfo(this);
+});
+
+document.getElementById('hqdefault').addEventListener('click', function() {
+  setImageInfo(this);
+});
+
+document.getElementById('mqdefault').addEventListener('click', function() {
+  setImageInfo(this);
 });
 
 document.getElementById('download').addEventListener('click', function() {
   chrome.downloads.download({
-    filename: title + '.jpg',
+    filename: document.getElementById('title').value + '.jpg',
     url: this.href
   });
 });
